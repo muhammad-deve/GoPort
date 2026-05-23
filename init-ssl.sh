e399d7f311b4cc32c418294021445b493e4c710d
Muhammad <muhammadjonxudaynazarov226@gmail.com>
2026-05-23 21:42:53 +0500

    Changed nginx

--- init-ssl.sh
+++ init-ssl.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-# Initial SSL certificate setup for goport.uz
-# Run this script ONCE on your server before starting docker-compose
+# Get SSL certificate for goport.uz using the existing barbershop certbot
+# Run this ONCE from the barbershop project directory (where certbot volumes are)
 
 DOMAIN="goport.uz"
 EMAIL="admin@goport.uz"  # Change this to your email
 
-echo ">>> Creating required directories..."
-mkdir -p ./certbot/conf
-mkdir -p ./certbot/www
-
-echo ">>> Stopping barbershop-nginx to free port 80/443..."
-docker stop barbershop-nginx
-
-echo ">>> Starting nginx with init config for ACME challenge..."
-# Temporarily use init config
-cp ./nginx/nginx-init.conf ./nginx/nginx.conf.bak
-cp ./nginx/nginx-init.conf ./nginx/nginx.conf
-
-docker compose up -d nginx
-
-echo ">>> Requesting SSL certificate from Let's Encrypt..."
-docker compose run --rm certbot certonly \
+echo ">>> Requesting SSL certificate for $DOMAIN..."
+docker exec barbershop-certbot certbot certonly \
   --webroot \
   --webroot-path=/var/www/certbot \
   --email $EMAIL \
@@ -31,13 +17,7 @@ docker compose run --rm certbot certonly \
   -d www.$DOMAIN \
   -d back.$DOMAIN
 
-echo ">>> Restoring full nginx config..."
-cp ./nginx/nginx.conf.bak ./nginx/nginx.conf
-rm ./nginx/nginx.conf.bak
+echo ">>> Reloading nginx..."
+docker exec barbershop-nginx nginx -s reload
 
-echo ">>> Restarting all services..."
-docker compose down
-docker compose up -d
-
-echo ">>> Done! SSL certificate installed for $DOMAIN and back.$DOMAIN"
-echo ">>> Note: barbershop-nginx is stopped. Start it again if needed on different ports."
+echo ">>> Done! Now copy goport.conf into your barbershop nginx config directory and reload again."
