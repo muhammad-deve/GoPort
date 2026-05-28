e9f66c92435343cdec456fea729a33fac952f7a3
Muhammad <muhammadgo.deve@gmail.com>
2026-05-28 23:26:16 +0500

    Some changes in subnidukes

--- init-ssl.sh
+++ init-ssl.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Get SSL certificate for goport.uz using the existing barbershop certbot
-# Run this ONCE from the barbershop project directory (where certbot volumes are)
+# Get SSL certificate for goport.uz and *.goport.uz using the existing
+# Barbershop certbot container.
+#
+# Wildcard certificates require DNS validation. Certbot will print a TXT record
+# that you must add in your DNS panel before continuing.
 
 DOMAIN="goport.uz"
 EMAIL="admin@goport.uz"  # Change this to your email
 
-echo ">>> Requesting SSL certificate for $DOMAIN..."
-docker exec barbershop-certbot certbot certonly \
-  --webroot \
-  --webroot-path=/var/www/certbot \
+echo ">>> Requesting wildcard SSL certificate for $DOMAIN..."
+docker exec -it barbershop-certbot certbot certonly \
+  --manual \
+  --preferred-challenges dns \
   --email $EMAIL \
   --agree-tos \
   --no-eff-email \
   -d $DOMAIN \
-  -d www.$DOMAIN \
-  -d back.$DOMAIN
+  -d "*.$DOMAIN"
 
 echo ">>> Reloading nginx..."
 docker exec barbershop-nginx nginx -s reload
 
-echo ">>> Done! Now copy goport.conf into your barbershop nginx config directory and reload again."
+echo ">>> Done! Copy nginx/goport.conf into your Barbershop nginx config and reload nginx again."
